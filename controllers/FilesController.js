e7202199394231a3b0b11b1af779c068be397fb3
David-Inkheart <okoliedave1@gmail.com>
2023-06-25 08:05:21 +0100

    fixed bug in postUpload

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -8,71 +8,66 @@ import redisClient from '../utils/redis';
 const writeFileAsync = promisify(fs.writeFile);
 
 class FilesController {
+  // eslint-disable-next-line consistent-return
   static async postUpload(req, res) {
-    const tokenValue = req.headers['x-token'];
-    // console.log(tokenValue);
-    if (!tokenValue) return res.status(401).json({ error: 'Unauthorized' });
-    // use token.js to retrieve the user ID from the token
-    const userId = await redisClient.get(`auth_${tokenValue}`);
-    // console.log(userId);
-    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
-    const user = await dbClient.findUser({ _id: ObjectId(userId) });
-    // console.log(user);
-    if (!user) return res.status(401).json({ error: 'Unauthorized' });
-    // return the user information
-    const {
-      name, type, parentId, isPublic, data,
-    } = req.body;
-    if (!name) return res.status(400).json({ error: 'Missing name' });
-    if (!type || !['folder', 'file', 'image'].includes(type)) return res.status(400).json({ error: 'Missing type' });
-    if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
-    if (parentId) {
-      const parent = await dbClient.findFile({ _id: ObjectId(parentId) });
-      if (!parent) return res.status(400).json({ error: 'Parent not found' });
-      if (parent.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
+    const token = req.header('X-Token') || null;
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    if (!req.body.name) return res.status(400).send({ error: 'Missing name' });
+    if (!req.body.type || !['folder', 'file', 'image'].includes(req.body.type)) {
+      return res.status(400).send({ error: 'Missing type' });
     }
+
+    const { name } = req.body;
+    const { type } = req.body;
+    const parentId = req.body.parentId ? ObjectId(req.body.parentId) : 0;
+    const isPublic = req.body.isPublic || false;
+    const data = req.body.data || null;
+    const filePath = process.env.FOLDER_PATH || '/tmp/files_manager';
+
     if (type === 'folder') {
-      const newFolder = await dbClient.createFile({
+      const folder = await dbClient.db.collection('files').insertOne({
         userId: ObjectId(userId),
         name,
         type,
-        isPublic: isPublic || false,
-        parentId: parentId || 0,
+        isPublic,
+        parentId,
       });
-      return res.status(201).json({
-        id: newFolder.insertedId,
+      return res.status(201).send({
+        id: folder.insertedId,
         userId: user._id,
         name,
         type,
-        isPublic: isPublic || false,
-        parentId: parentId || 0,
+        isPublic,
+        parentId,
       });
-    }
-    const buff = Buffer.from(data, 'base64');
-    const path = process.env.FOLDER_PATH || '/tmp/files_manager';
-    const filePath = `${path}/${uuidv4()}`;
+      // eslint-disable-next-line no-else-return
+    } else if (['file', 'image'].includes(type)) {
+      if (!data) return res.status(400).send({ error: 'Missing data' });
+      const buff = Buffer.from(data, 'base64');
+      const fileName = uuidv4();
+      const localPath = `${filePath}/${fileName}`;
 
-    try {
-      await writeFileAsync(filePath, buff);
-      const newFile = await dbClient.createFile({
+      await writeFileAsync(localPath, buff);
+
+      const file = await dbClient.db.collection('files').insertOne({
         userId: ObjectId(userId),
         name,
         type,
-        isPublic: isPublic || false,
-        parentId: parentId || 0,
-        localPath: filePath,
+        isPublic,
+        parentId,
+        localPath,
       });
-
-      return res.status(201).json({
-        id: newFile.insertedId,
+      return res.status(201).send({
+        id: file.insertedId,
         userId: user._id,
         name,
         type,
-        isPublic: isPublic || false,
-        parentId: parentId || 0,
+        isPublic,
+        parentId,
       });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
     }
   }
 }
